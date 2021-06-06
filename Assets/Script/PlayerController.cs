95f1700c0b6e9f7eac1b83d62027e579003c7fe3
一腔诗意在哪 <zaze97@163.com>
2021-06-06 22:32:36 +0800

    player脚本制作

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    private Camera cam;
+    PlayerMotor motor;
+
+    public Interactable focus;
+
+    public LayerMask movementMask;
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit,100, movementMask))
+            {
+                Debug.Log("µã»÷ÎïÌå"+hit.collider.name + "===" + hit.point);
+                motor.MoveToPoint(hit.point);
+
+                RemoveFocus();
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, 100))
+            {
+                Interactable interactanle = hit.collider.GetComponent<Interactable>();
+                if (interactanle != null)
+                {
+                    SetFocus(interactanle);
+                }
+            }
+        }
+    }
+
+    private void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+                focus.OnDeFocused();
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+        newFocus.OnFocused(transform);
+    }
+    private void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDeFocused();
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+}
---
+++ Assets/Script/PlayerMotor.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour
+{   
+    NavMeshAgent agent;
+    Transform target;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+
+    }
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        //改变自动寻路跟物体的半径距离
+        agent.stoppingDistance = newTarget.radius * 0.8f;
+        //避免当有间隔时物体不会跟随目标点旋转
+        agent.updateRotation = false;
+        target = newTarget.InteractableTran;
+    }
+    public void StopFollowingTarget()
+    {
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true;
+        target = null;
+    }
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        //旋转插值，让旋转更平滑
+        transform.rotation = Quaternion.Slerp(transform.rotation,lookRotation,Time.deltaTime*5f);
+    }
+}
