ac87912b886860dc8a011ce6e6a18fb85d417370
一腔诗意在哪 <zaze97@163.com>
2021-06-11 22:40:04 +0800

    动画制作

---
+++ Assets/Script/FollowTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    private Transform player;
+    private Vector3 offset;
+    private float smoothing = 3;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        //player.TransformDirection( offset)
+        //将指定的值转为Player的局部坐标
+        Vector3 targetPosition = player.position + player.TransformDirection( offset)  ;
+        transform.position = Vector3.Lerp(transform.position, targetPosition,Time.deltaTime*smoothing);
+        transform.LookAt(player.position);
+	}
+}
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Animator anim;
+
+    //BlendTreeZ方向动画的阈值
+    private float BlendMove=5.7f;
+    private bool Switching=false;
+    private float SwitchingTime = 0;
+    private int speedZID = Animator.StringToHash("MoveZ");
+    private int speedRotateID = Animator.StringToHash("SpeedRotate");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        float v = 0, h = 0;
+        v = Input.GetAxis("Vertical");
+       // if (v >= 4.38f/ BlendMove)
+      //  {
+            h = Input.GetAxis("Horizontal");
+       // }
+        /*        if (v != 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("Locomotion"))
+                {
+                    SwitchingTime +=Time.deltaTime;
+                    if (SwitchingTime >= 2)
+                    {
+                        Switching = true;
+                    }
+                }
+                else
+                {
+                    SwitchingTime = 0;
+                    Switching = false;
+                }
+                BlendMove=v>=1&&?*/
+        anim.SetFloat(speedZID, v * BlendMove);
+        anim.SetFloat(speedRotateID, h * 46);
+        //anim.SetFloat(speedRotateID, Input.GetAxis("Horizontal") * 126);
+        //transform.Translate(h * Time.deltaTime, 0, v*Time.deltaTime);
+    }
+}
